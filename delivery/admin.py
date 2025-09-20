770b74435812065ecfea996912af1b1397f085a1
Nancylak <nylac966@gmail.com>
2025-09-21 00:25:30 +0530

    Initial 20th sept

--- delivery/admin.py
+++ delivery/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import Customer
 from .models import Restaurant
+from .models import Item
+from .models import Cart
 
 # Register your models here.
 admin.site.register(Customer)
 admin.site.register(Restaurant)
+admin.site.register(Item)
+admin.site.register(Cart)
